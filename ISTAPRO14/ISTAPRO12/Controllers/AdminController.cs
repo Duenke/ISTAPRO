cb888702050e795df297c9c9d4985351eedd0dd6
Jacob Duenke <jacob.duenke@outlook.com>
2019-04-29 08:27:16 -0400

    Completed step 14.

---
+++ ISTAPRO14/ISTAPRO12/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using ISTAPRO12.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ISTAPRO12.Controllers
+{
+    public class AdminController : Controller
+    {
+        public TicketRepo controllerRepo = TicketRepo.updateableRepo;
+
+        public IActionResult Index() => View();
+
+        public IActionResult NewTickets() => View(controllerRepo.Tickets.OrderBy(t => t.TicketNum));
+
+        public IActionResult ViewTicket(int ticketNum) => View(controllerRepo.Tickets.FirstOrDefault(t => t.TicketNum == ticketNum));
+    }
+}
---
+++ ISTAPRO14/ISTAPRO12/Models/Ticket.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ISTAPRO12.Models
+{
+    public class Ticket
+    {
+        // Possible Ticket properties.
+        public int TicketNum { get; set; }
+        public string SubmitDate { get; set; }
+        public string OpenDate { get; set; }
+        public string CloseDate { get; set; }
+        public int EmpID { get; set; }
+        public int CustID { get; set; }
+        [Required(ErrorMessage = "Please enter a full name")]
+        public string CustName { get; set; }
+        [Required(ErrorMessage = "Please enter an email address")]
+        public string CustEmail { get; set; }
+        [Required(ErrorMessage = "Please enter a device or software causing the issue")]
+        public string Platform { get; set; }
+        [Required(ErrorMessage = "Please enter a brief summary of the issue")]
+        public string Summary { get; set; }
+        [Required(ErrorMessage = "Please enter a detailed description of the issue")]
+        public string Issue { get; set; }
+        public string Resolution { get; set; }
+    }
+}
